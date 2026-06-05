a8c64ce1823fd24b781336a926997d1dfba1de12
benjaminmcinneswrites <benjaminmcinneswrites@gmail.com>
2026-06-05 21:50:50 +1200

    Harden public site assets

--- contact-form-config.js
+++
@@ -1,8 +0,0 @@
-/*
-  Contact form email delivery config.
-  Replace recipientEmail with the inbox that should receive contact messages.
-*/
-window.BLB_CONTACT_FORM_CONFIG = {
-  recipientEmail: "contact@bottlelakebikes.co.nz",
-  subject: "New contact message from Bottle Lake Bikes website"
-};
